ed355cdbe72c7ceeb370ec59994437b5c2836c17
onesuper <onesuperclark@gmail.com>
2017-05-30 10:23:06 +0800

    release 3.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ extras_require = {
 
 setup(
     name='pandasticsearch',
-    version='0.3.1',
+    version='0.3.2',
     author='onesuper',
     author_email='onesuperclark@gmail.com',
     packages=['pandasticsearch'],
