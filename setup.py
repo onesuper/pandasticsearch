84a1cb80e35d4caa6caa5a121b5cce1d69b5225f
onesuper <onesuperclark@gmail.com>
2017-09-17 11:19:53 +0800

    release 0.4.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ extras_require = {
 
 setup(
     name='pandasticsearch',
-    version='0.4.1',
+    version='0.4.2',
     author='onesuper',
     author_email='onesuperclark@gmail.com',
     packages=['pandasticsearch'],
