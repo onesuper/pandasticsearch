c1dab192a4577f542daa371bc29e5f3e169f9d9d
onesuper <onesuperclark@gmail.com>
2020-05-02 23:43:57 +0800

    release 0.5.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ extras_require = {
 
 setup(
     name='pandasticsearch',
-    version='0.5.3',
+    version='0.5.5',
     author='onesuper',
     author_email='onesuperclark@gmail.com',
     packages=['pandasticsearch', 'pandasticsearch.operators'],
